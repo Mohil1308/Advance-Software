5c64d64afa9b8aaaac55b9fbc1c284402bce8bab
Mohil1308 <mohil1997jariwala@gmail.com>
2023-10-30 13:14:09 +0000

    Completed The DrawShape

--- Advance Software first module/DrawShape.cs
+++ Advance Software first module/DrawShape.cs
@@ -1,14 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Advance_Software_first_module
 {
-    internal class DrawShape
+    public class DrawShape
     {
-
+        private static int _a, _b;
+        static private Bitmap _NPicture = new Bitmap(680, 450);
+        static SolidBrush _FillColor;
+        static Boolean _isFill;
+        static Boolean _isUnitValid;
+        static Color _penColor;
+        public static Boolean isFill
+        {
+            get
+            {
+                return _isFill;
+            }
+            set { _isFill = value; }
+        }
+        public static SolidBrush FillColor
+        {
+            get
+            {
+                return _FillColor;
+            }
+            set { _FillColor = value; }
+        }
+        public static Bitmap NPicture
+        {
+            get
+            {
+                return _NPicture;
+            }
+            set { _NPicture = value; }
+        }
+        public static int a
+        {
+            get
+            {
+                return _a;
+            }
+            set { _a = value; }
+        }
+        public static int b
+        {
+            get
+            {
+                return _b;
+            }
+            set { _b = value; }
+        }
+        public static Color penColor
+        {
+            get
+            {
+                return _penColor;
+            }
+            set { _penColor = value; }
+        }
+        public static Boolean isUnitValid
+        {
+            get
+            {
+                return _isUnitValid;
+            }
+            set { _isUnitValid = value; }
+        }
     }
 }
