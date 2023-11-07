e20e22a05a13952078a0f5686f783ffccc3f0393
Mohil1308 <mohil1997jariwala@gmail.com>
2023-11-07 17:32:12 +0000

    Unit Test Done

---
+++ Unittest1/UnitTest1.cs
@@ -0,0 +1,49 @@
+using System;
+using ASE__ASSINGMENT;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unittest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        DrawingControl ctrl = new DrawingControl();
+
+        [TestMethod]
+        public void TestDrawLine()
+        {
+            ctrl.DrawLine(100, 300);
+            Assert.IsTrue(DrawingShapes.isUnitValid);
+        }
+        //Method for unit test to Draw Square
+        [TestMethod]
+        public void TestSquare()
+        {
+            ctrl.DrawSquare(25);
+            Assert.IsTrue(DrawingShapes.isUnitValid);
+        }
+        //Method for unit to Draw Circle
+        [TestMethod]
+        //Method for unit test to Move pen
+        public void TestMove()
+        {
+            ctrl.MovePoint(20, 100);
+            Assert.IsTrue(DrawingShapes.isUnitValid);
+        }
+
+        //Method for unit test to Draw Triangle
+        [TestMethod]
+        public void TestTriangle()
+        {
+            ctrl.DrawTriangle(120, 150, 170);
+            Assert.IsTrue(DrawingShapes.isUnitValid);
+        }
+        //Method for unit test to Draw Rectangle
+        [TestMethod]
+        public void TestRectangle()
+        {
+            ctrl.DrawRect(100, 300);
+            Assert.IsTrue(DrawingShapes.isUnitValid);
+        }
+    }
+}
