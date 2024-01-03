4f3a033ebac1c16a5f6af7b587aa5da90f3df2b7
Mohil1308 <mohil1997jariwala@gmail.com>
2024-01-03 03:02:30 +0000

    Implemented drawSquare Class

--- Section 2/drawSquare.cs
+++ Section 2/drawSquare.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Section_2
 {
-    public class drawSquare
+    class DrawSquare : DrawRectangle
     {
+        readonly int size;
+        public DrawSquare(int x, int y, int size) : base(x, y, size, size)
+        {
+
+            this.size = size;
+        }
+
+
+        public override void Draw(Graphics g, Pen pen, Brush brush)
+        {
+            base.Draw(g, pen, brush);
+        }
+
     }
 }
