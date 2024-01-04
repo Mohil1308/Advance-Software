05534a5d317589d36ea2fa82de52e7f81a8a1204
Mohil1308 <mohil1997jariwala@gmail.com>
2024-01-04 11:42:46 +0000

    Implemented drawtriangle class.

--- Section 2/Form1.Designer.cs
+++ Section 2/Form1.Designer.cs
@@ -28,60 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
             button3 = new Button();
-            tabControl1 = new TabControl();
+            button4 = new Button();
+            button2 = new Button();
+            ControlPanel = new TabControl();
             groupBox1 = new GroupBox();
-            pictureBox1 = new PictureBox();
+            display = new PictureBox();
             groupBox2 = new GroupBox();
             groupBox3 = new GroupBox();
-            tabControl2 = new TabControl();
+            commandline = new TabControl();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)display).BeginInit();
             groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // button3
             // 
-            button1.Location = new Point(31, 938);
-            button1.Name = "button1";
-            button1.Size = new Size(150, 46);
-            button1.TabIndex = 0;
-            button1.Text = "Run";
-            button1.UseVisualStyleBackColor = true;
+            button3.Location = new Point(31, 938);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 46);
+            button3.TabIndex = 0;
+            button3.Text = "Run";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(296, 938);
+            button4.Name = "button4";
+            button4.Size = new Size(150, 46);
+            button4.TabIndex = 1;
+            button4.Text = "Clear";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button2_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(296, 938);
+            button2.Location = new Point(559, 938);
             button2.Name = "button2";
             button2.Size = new Size(150, 46);
-            button2.TabIndex = 1;
-            button2.Text = "Clear";
+            button2.TabIndex = 2;
+            button2.Text = "Save";
             button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
             // 
-            // button3
+            // ControlPanel
             // 
-            button3.Location = new Point(559, 938);
-            button3.Name = "button3";
-            button3.Size = new Size(150, 46);
-            button3.TabIndex = 2;
-            button3.Text = "Save";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // tabControl1
-            // 
-            tabControl1.Location = new Point(31, 38);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(657, 622);
-            tabControl1.TabIndex = 3;
+            ControlPanel.Location = new Point(31, 38);
+            ControlPanel.Name = "ControlPanel";
+            ControlPanel.SelectedIndex = 0;
+            ControlPanel.Size = new Size(657, 622);
+            ControlPanel.TabIndex = 3;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(tabControl1);
+            groupBox1.Controls.Add(ControlPanel);
             groupBox1.Location = new Point(21, 97);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(711, 691);
@@ -89,17 +89,17 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Multi Command";
             // 
-            // pictureBox1
+            // display
             // 
-            pictureBox1.Location = new Point(21, 42);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(900, 849);
-            pictureBox1.TabIndex = 5;
-            pictureBox1.TabStop = false;
+            display.Location = new Point(21, 42);
+            display.Name = "display";
+            display.Size = new Size(900, 849);
+            display.TabIndex = 5;
+            display.TabStop = false;
             // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(pictureBox1);
+            groupBox2.Controls.Add(display);
             groupBox2.Location = new Point(754, 12);
             groupBox2.Name = "groupBox2";
             groupBox2.Size = new Size(927, 907);
@@ -110,7 +110,7 @@
             // 
             // groupBox3
             // 
-            groupBox3.Controls.Add(tabControl2);
+            groupBox3.Controls.Add(commandline);
             groupBox3.Location = new Point(754, 938);
             groupBox3.Name = "groupBox3";
             groupBox3.Size = new Size(927, 124);
@@ -118,13 +118,13 @@
             groupBox3.TabStop = false;
             groupBox3.Text = "Single Command";
             // 
-            // tabControl2
+            // commandline
             // 
-            tabControl2.Location = new Point(21, 58);
-            tabControl2.Name = "tabControl2";
-            tabControl2.SelectedIndex = 0;
-            tabControl2.Size = new Size(900, 44);
-            tabControl2.TabIndex = 6;
+            commandline.Location = new Point(21, 58);
+            commandline.Name = "commandline";
+            commandline.SelectedIndex = 0;
+            commandline.Size = new Size(900, 44);
+            commandline.TabIndex = 6;
             // 
             // Form1
             // 
@@ -135,14 +135,14 @@
             Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Controls.Add(button3);
             Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)display).EndInit();
             groupBox2.ResumeLayout(false);
             groupBox3.ResumeLayout(false);
             ResumeLayout(false);
@@ -150,14 +150,14 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
         private Button button3;
-        private TabControl tabControl1;
+        private Button button4;
+        private Button button2;
+        private TabControl ControlPanel;
         private GroupBox groupBox1;
-        private PictureBox pictureBox1;
+        private PictureBox display;
         private GroupBox groupBox2;
         private GroupBox groupBox3;
-        private TabControl tabControl2;
+        private TabControl commandline;
     }
 }
--- Section 2/drawTriangle.cs
+++ Section 2/drawTriangle.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Section_2
 {
-    public class drawTriangle
+    public class drawTriangle : Shape
     {
+        private Point[] point;
 
+        public drawTriangle(Point[] point)
+        {
+            this.point = point;
+        }
+
+        public drawTriangle() { }
+
+        public override void setTriangle(int x, int y, Point[] points)
+        {
+            base.set(x, y);
+            this.point = points;
+        }
+
+        public override void Draw(Graphics graphic, Pen pen, Brush brush)
+        {
+            graphic.DrawPolygon(pen, point);
+            graphic.FillPolygon(brush, point);
+        }
     }
 }
