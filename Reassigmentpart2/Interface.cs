c2e07232e1d20280d3514418bd55664da8604985
Mohil1308 <mohil1997jariwala@gmail.com>
2024-03-03 12:35:44 +0000

    Define interface for shape drawing

--- Reassigmentpart2/Interface.cs
+++ Reassigmentpart2/Interface.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Reassigmentpart2
 {
-    internal class Interface
+    public interface IShape
     {
+        // Define methods for setting shape parameters
+        void SetParameters(params int[] parameters);
+
+        // Define methods for setting specific parameters for a triangle
+        void SetTriangleParameters(int x, int y, Point[] points);
+
+        // Define method for drawing the shape
+        void DrawShape(Graphics graphics, Pen pen, Brush brush);
     }
 }
